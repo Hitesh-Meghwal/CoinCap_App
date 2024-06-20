2a273f0ef3d8ae57811154c3430872591a22e8ac
Hitesh-Meghwal <21sycshiteshmeghwal@gmail.com>
2024-06-20 10:03:00 +0530

    Inital Commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _homePage();
+  }
+}
+
+class _homePage extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+  }
